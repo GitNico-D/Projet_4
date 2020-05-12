592f1f7b4c043f23dc9201cd8784b3be159bb68c
Nicolas <niicolas.dubief@gmail.com>
2020-05-12 19:59:36 +0200

    Constant Connection DB

---
+++ config/PDOConnection.php
@@ -0,0 +1,8 @@
+<?php
+
+CONST HOST = 'localhost';
+CONST DB_CHARSET = 'utf8';
+CONST DB_NAME = 'db_project_four';
+CONST DB_HOST = 'mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=' . DB_CHARSET;
+CONST DB_USERNAME = 'root';
+CONST DB_PASSWORD = '';
