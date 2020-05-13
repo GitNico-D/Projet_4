2f995cf3714c90976741a9dbc58c5d5d728b7a2d
Nicolas <niicolas.dubief@gmail.com>
2020-05-13 17:01:38 +0200

    Article Manager

---
+++ src/DAO/ArticleManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\src\DAO;
+
+class ArticleManager extends DAO
+{
+
+    public function getUniqueArticle($articleId)
+    {
+        $sqlRequest = 'SELECT * FROM news WHERE id = ?';
+        var_dump($articleId);
+        return $this->createQuery($sqlRequest, [$articleId]);
+    }
+
+    public function getAllArticles()
+    {
+        $sqlRequest = 'SELECT * FROM news ORDER BY creationDate ASC';
+        return $this->createQuery($sqlRequest);
+    }
+
+    public function addNewArticle(Article $article)
+    {
+        $sqlRequest = 'INSERT INTO news(title, author, content, creationDate) VALUES (:author, :title, :content, NOW())';
+        return $this->createQuery($sqlRequest, [$article->setTitle(), ])
+    }
+}
