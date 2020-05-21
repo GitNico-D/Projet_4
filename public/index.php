63ceaacc51cf2d735d049df3f3cfdf27c13cf144
Nicolas <niicolas.dubief@gmail.com>
2020-05-21 19:20:37 +0200

    Simplification of index

--- public/index.php
+++ public/index.php
@@ -1,45 +1,9 @@
 <?php 
 
-// require '../config/PDOConnection.php';
 require '../vendor/autoload.php';
-use App\src\controller\FrontController;
-use App\src\controller\ErrorController;
-// use Exception;
+require '../src/controller/Router.php';
 
-$frontController = new FrontController();
-$errorController = new ErrorController();
-
-try 
-{
-    if (isset($_GET['page']))
-    {
-    $page = $_GET['page'];
-        switch ($page)
-        {
-            case 'single': 
-                $frontController->single($_GET['articleId']);
-            break;
-            case 'addArticle':
-                $frontController->addArticle($_POST);
-            break;
-            case 'getLogin':
-                $frontController->logIn();
-            break;
-            default:
-                $errorController->errorPageNotFound();
-        }
-    }
-    else 
-    {
-        $frontController->home();
-    }
-}
-catch (Exception $errorConnexion)
-{
-    // $errorMessage = $errorConnexion->getMessage();
-    $errorController->PageNotFound($errorMessage);
-}
-
-    
+$router = new Router();
+$router->launchApp();    
 ?>      
  
