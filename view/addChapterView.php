fba8443e6ccc18e8edf5e88867652826549c6a72
Nicolas <niicolas.dubief@gmail.com>
2020-05-28 18:06:11 +0200

    Testing of addChapterView

--- view/addChapterView.php
+++ view/addChapterView.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -20,19 +18,41 @@
         </div>
         <section class="jumbotron text-center">
             <div class="container">
-                <h1 class="jumbotron-heading">Gestion des chapters !</h1>
+                <h1 class="jumbotron-heading">Gestion des chapitres !</h1>
             </div>
-            <p class="lead text-muted">Cette page permet d'écrire et d'ajouter un nouvel chapter.</p>
-                <h3> Formulaire d'ajout d'un chapter !</h3>
-                <form action="index.php?page=addchapter" method="post">
+            <p class="lead text-muted">Cette page permet d'écrire et d'ajouter un nouvel chapitre.</p>
+                <h3> Formulaire d'ajout d'un chapitre !</h3>
+                <form action="index.php?page=addChapter" method="post">
                     <div class="form-group">
-                    <label for="title">Titre de l'chapter</label>
-                    <input type="text" class="form-control" id="Title" name="Title">
-                    <label for="content">Contenu de l'chapter</label>
-                    <textarea type="text" class="form-control" id="Content" name="Content" rows="10"></textarea>
-                    <button type="submit" class="btn btn-primary">Envoyer chapter</button>
+                    <label for="chapterAuthor">Auteur du chapitre</label>
+                    <input type="text" class="form-control" id="chapterAuthor" name="chapterAuthor">
+                    <label for="chapterTitle">Titre du chapitre</label>
+                    <input type="text" class="form-control" id="chapterTitle" name="chapterTitle">
+                    <label for="chapterContent">Contenu du chapitre</label>
+                    <textarea type="text" class="form-control" id="chapterContent" name="chapterContent" rows="10"></textarea>
+                    <!-- <input type="text" name="id" value=""/> -->
+                    <button type="submit" class="btn btn-primary">Envoyer chapitre</button>
+                    </div>
                 </form>
                 <div><a href="../public/index.php">Retour à Home Page</a></div>
         </section>
     </body>
 </html>
+<!-- <?= $pageTitle = 'Ajout d\'un nouveau chapitre !'?>
+<?php ob_start(); ?>
+    <form action="index.php?page=addChapter" method="post">
+            <div class="form-group">
+            <label for="chapterAuthor">Auteur du chapitre</label>
+            <input type="text" class="form-control" id="chapterAuthor" name="chapterAuthor">
+            <label for="chapterTitle">Titre du chapitre</label>
+            <input type="text" class="form-control" id="chapterTitle" name="chapterTitle">
+            <label for="chapterContent">Contenu du chapitre</label>
+            <textarea type="text" class="form-control" id="chapterContent" name="chapterContent" rows="10"></textarea>
+            <input type="text" name="id" value="<?= $chapter->getChapterId();?>"/>
+            <button type="submit" class="btn btn-primary">Envoyer chapitre</button>
+            </div>
+        </form>
+        <div><a href="../public/index.php">Retour à Home Page</a></div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'defaultView.php'; ?> -->
