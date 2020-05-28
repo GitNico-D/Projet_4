541835741a022f1f6621c39c63dfb613483ac035
Nicolas <niicolas.dubief@gmail.com>
2020-05-28 18:04:42 +0200

    Change "Article" in " Chapter"

--- view/homeView.php
+++ view/homeView.php
@@ -3,13 +3,12 @@
 <p class="lead text-muted">Voici tous les chapitres disponibles !</p>
 <?php foreach($chaptersList as $chapter) :?>
 <div class=card>
-    <h2 class=card-header><?= htmlspecialchars($chapter->getTitle())?></h2>
-    <p class=card-body>Posté le <span class=font-italic><?= htmlspecialchars($chapter->getCreateDate())?></span> 
-        par <span class=font-italic><?= htmlspecialchars($chapter->getAuthor())?></span></p>
-    <p class= text-center-justify><?= htmlspecialchars($chapter->getContent())?></p>
-    <a href="../public/index.php?page=single&chapterId=<?= htmlspecialchars($chapter->getId())?>">En savoir plus... </a>
+    <h2 class=card-header><?= htmlspecialchars($chapter->getChapterTitle())?></h2>
+    <p class=card-body>Posté le <span class=font-italic><?= htmlspecialchars($chapter->getChapterCreateDate())?></span> 
+        par <span class=font-italic><?= htmlspecialchars($chapter->getChapterAuthor())?></span></p>
+    <p class= text-center-justify><?= htmlspecialchars($chapter->getChapterContent())?></p>
+    <a href="../public/index.php?page=single&chapterId=<?= htmlspecialchars($chapter->getChapterId())?>">En savoir plus... </a>
 </div>
 <?php endforeach; ?>
 <?php $content = ob_get_clean(); ?>
-
 <?php require 'defaultView.php'; ?>
--- view/singleView.php
+++ view/singleView.php
@@ -1,13 +1,13 @@
 <?= $pageTitle = 'Affichage d\'un unique chapter !'?>
 <?php ob_start(); ?>
-    <p class="lead text-muted">Voici le chapitre <?php echo $uniqueChapter->getId();?></p>
+    <p class="lead text-muted">Voici le chapitre <?php echo $uniqueChapter->getChapterId();?></p>
     <div class=card>
-        <h2 class=card-header font-weight-bold><?= htmlspecialchars($uniqueChapter->getTitle());?></h2>
-        <p class=card-body>Posté le <span class=font-italic><?= htmlspecialchars($uniqueChapter->getCreateDate());?></span> 
-            par <span class=font-italic><?= htmlspecialchars($uniqueChapter->getAuthor());?></span></p>
-        <p class= text-center-justify><?= htmlspecialchars($uniqueChapter->getContent());?></p>
+        <h2 class=card-header font-weight-bold><?= htmlspecialchars($uniqueChapter->getChapterTitle());?></h2>
+        <p class=card-body>Posté le <span class=font-italic><?= htmlspecialchars($uniqueChapter->getChapterCreateDate());?></span> 
+            par <span class=font-italic><?= htmlspecialchars($uniqueChapter->getChapterAuthor());?></span></p>
+        <p class= text-center-justify><?= htmlspecialchars($uniqueChapter->getChapterContent());?></p>
         <a href="../public/index.php">Retour à Home Page</a>
     </div>
 <?php $content = ob_get_clean(); ?>
 
-<?php require 'defaultView.php' ?>
+<?php require 'defaultView.php'; ?>
