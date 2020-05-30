bf8b7978397a84a397a3b53a4b62b28770353a0e
Nicolas <niicolas.dubief@gmail.com>
2020-05-30 20:16:24 +0200

    Try to add comment on singleView page

--- view/singleView.php
+++ view/singleView.php
@@ -13,6 +13,18 @@
         <p class= text-center-justify><?= htmlspecialchars($uniqueChapter->getChapterContent());?></p>
         <a href="../public/index.php">Retour à Home Page</a>
     </div>
+    <div class="container">
+    <?php var_dump($commentList); foreach($commentList as $comment) :?>
+        <div class="card">
+        <h4 class="card-body">Commentaire</h4>
+        <p class=card-body>Posté le <span class=font-italic><?= htmlspecialchars($comment->getCommentCreatedDate());?></span> 
+            par <span class=font-italic><?= htmlspecialchars($comment->getCommentAuthor());?></span></p>
+        <p class= text-center-justify><?= htmlspecialchars($comment->getCommentContent());?></p>
+        <p class= text-center-justify><?= htmlspecialchars($comment->getChapterId());?></p>
+        </div>
+    </div>
+    <?php endforeach; ?>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require 'defaultView.php'; ?>
