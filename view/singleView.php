301aabc4be3530abbf789d948ced20ef85a34b5c
Nicolas <niicolas.dubief@gmail.com>
2020-06-01 18:34:30 +0200

    Display change of comments

--- view/singleView.php
+++ view/singleView.php
@@ -8,22 +8,22 @@
     <p class="lead text-muted">Voici le chapitre <?php echo $uniqueChapter->getChapterId();?></p>
     <div class=card>
         <h2 class=card-header font-weight-bold><?= htmlspecialchars($uniqueChapter->getChapterTitle());?></h2>
-        <p class=card-body>Posté le <span class=font-italic><?= htmlspecialchars($uniqueChapter->getChapterCreateDate());?></span> 
-            par <span class=font-italic><?= htmlspecialchars($uniqueChapter->getChapterAuthor());?></span></p>
+        <p class=card-body>Posté le <span class="font-italic"><?= htmlspecialchars($uniqueChapter->getChapterCreateDate());?></span> 
+            par <span class="font-italic"><?= htmlspecialchars($uniqueChapter->getChapterAuthor());?></span></p>
         <p class= text-center-justify><?= htmlspecialchars($uniqueChapter->getChapterContent());?></p>
         <a href="../public/index.php">Retour à Home Page</a>
     </div>
+    <hr>
     <div class="container">
-    <?php var_dump($commentList); foreach($commentList as $comment) :?>
-        <div class="card">
-        <h4 class="card-body">Commentaire</h4>
-        <p class=card-body>Posté le <span class=font-italic><?= htmlspecialchars($comment->getCommentCreatedDate());?></span> 
-            par <span class=font-italic><?= htmlspecialchars($comment->getCommentAuthor());?></span></p>
-        <p class= text-center-justify><?= htmlspecialchars($comment->getCommentContent());?></p>
-        <p class= text-center-justify><?= htmlspecialchars($comment->getChapterId());?></p>
-        </div>
+        <h4 class="text-center">Commentaire</h4>
+        <?php foreach($commentList as $comment) :?>
+            <div class="card">
+            <p class="card-header">Posté le <span class="font-italic"><?= htmlspecialchars($comment->getCommentCreatedDate());?></span> 
+                par <span class="font-italic bold"><?= htmlspecialchars($comment->getCommentAuthor());?></span></p>
+            <p class="card-body text-center-justify"><?= htmlspecialchars($comment->getCommentContent());?></p>
+            </div>
+        <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
 
 <?php $content = ob_get_clean(); ?>
 
