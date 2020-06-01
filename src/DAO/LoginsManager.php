4ef9105db1976841d115e54973be06fbb485ed16
Nicolas <niicolas.dubief@gmail.com>
2020-06-01 18:32:21 +0200

    Change the verification method of logins

--- src/DAO/LoginsManager.php
+++ src/DAO/LoginsManager.php
@@ -14,34 +14,34 @@ class LoginsManager extends DAO
         return $logins;
     }
 
-    public function loginsVerification()
-    {
-        $sqlRequest = 'SELECT loginsEmail, loginsPassword FROM logins';
-        $result = $this->createQuery($sqlRequest);
-        $loginsEmailList = [];
-        foreach($result as $logins)
-        {
-            $loginsEmailList [] = $this->createLoginsObject($logins);
-            // var_dump($loginsEmailList);
-        }
-        $result->closeCursor();
-        return $loginsEmailList;
-    }
-
-    // public function loginsPasswordVerification()
+    // public function loginsVerification()
     // {
-    //     $sqlRequest = 'SELECT loginsPassword FROM logins';
+    //     $sqlRequest = 'SELECT loginsEmail, loginsPassword FROM logins';
     //     $result = $this->createQuery($sqlRequest);
     //     $loginsEmailList = [];
     //     foreach($result as $logins)
     //     {
-    //         $loginsEmailList [] = $this->createloginsObject($logins);
+    //         $loginsEmailList [] = $this->createLoginsObject($logins);
     //         // var_dump($loginsEmailList);
     //     }
     //     $result->closeCursor();
     //     return $loginsEmailList;
     // }
 
+    public function loginsVerification($loginsEmail)
+    {
+        $sqlRequest = 'SELECT * FROM logins WHERE loginsEmail = ?';
+        $result = $this->createQuery($sqlRequest, [$loginsEmail]);
+        $loginsPassword = $result->fetch();
+        $result->closeCursor();
+        return $this->createLoginsObject($loginsPassword);
+    }
+
+    // public function passwordHash()
+    // {
+    //     return password_hash('AdminPassword', PASSWORD_DEFAULT);
+
+    // }
 
 
 
