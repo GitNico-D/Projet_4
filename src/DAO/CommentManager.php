38604d247bc40eb507667571b454b11807df6abf
Nicolas <niicolas.dubief@gmail.com>
2020-06-01 18:35:15 +0200

    NC

--- src/DAO/CommentManager.php
+++ src/DAO/CommentManager.php
@@ -14,18 +14,15 @@ class CommentManager extends DAO
 
     public function getCommentByChapterId($chapterId)
     {
-        var_dump($chapterId);
-        $sqlRequest = 'SELECT * FROM comments WHERE ? ORDER BY commentCreatedDate ASC';
+        $sqlRequest = 'SELECT * FROM comments WHERE chapterId = ? ORDER BY commentCreatedDate ASC';
         var_dump($sqlRequest);
         $result = $this->createQuery($sqlRequest, [$chapterId]);
-        var_dump($result);
         $commentList = [];
         foreach ($result as $comment)
         {
             $commentList [] = $this->createCommentObject($comment); 
         }
         $result->closeCursor();
-        // var_dump($commentList);
         return $commentList;
     }
 
