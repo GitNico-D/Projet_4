4137280f7cbf160181b2eb7242431e12a59e7e44
Nicolas <niicolas.dubief@gmail.com>
2020-06-05 19:03:48 +0200

    View of addChapter

---
+++ Simple/Views/addChapterView.php
@@ -0,0 +1,21 @@
+<?php
+    $pageTitle = 'Ajout d\'un nouveau chapitre';
+    include_once "./Views/Templates/header.php"; 
+?>
+
+<div class="container">
+    <form action="index.php?page=addNewChapter" method="post">
+            <div class="form-group">
+            <label for="chapterAuthor">Auteur du chapitre</label>
+            <input type="text" class="form-control" id="chapterAuthor" name="chapterAuthor">
+            <label for="chapterTitle">Titre du chapitre</label>
+            <input type="text" class="form-control" id="chapterTitle" name="chapterTitle">
+            <label for="chapterContent">Contenu du chapitre</label>
+            <textarea type="text" class="form-control" id="chapterContent" name="chapterContent" rows="10"></textarea>
+            <button type="submit" class="btn btn-primary">Envoyer chapitre</button>
+            </div>
+        </form>
+    </div>
+        <div><a href="./index.php">Retour à Home Page</a></div>
+
+<?php include_once './Views/Templates/Footer.php'; ?>
