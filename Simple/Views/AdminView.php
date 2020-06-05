a9afd8ab3790ae7055d95c3ee066617fb13e5b1d
Nicolas <niicolas.dubief@gmail.com>
2020-06-05 19:04:07 +0200

    View of Logins Form

---
+++ Simple/Views/AdminView.php
@@ -0,0 +1,42 @@
+<?php 
+    $pageTitle = 'Bienvenue Jean !'; 
+    include_once "./Views/Templates/header.php";
+?>
+
+
+<p class="lead text-muted">Voici la liste des chapitres que vous avez publiés !</p>
+<?php foreach($chaptersList as $chapter) {?>
+<div class=card>
+    <h2 class=card-header>
+        <?= htmlspecialchars($chapter->getTitle())?>
+    </h2>
+    <p class=card-body>
+        Posté le 
+        <span class=font-italic>
+            <?= htmlspecialchars($chapter->getCreateDate())?>
+        </span> 
+        par 
+        <span class=font-italic>
+            <?= htmlspecialchars($chapter->getAuthor())?>
+        </span>
+    </p>
+    <p class= text-center-justify>
+        <?= htmlspecialchars($chapter->getContent())?>
+    </p>
+    <div>
+        <a href="../public/index.php?page=single&chapterId=<?= htmlspecialchars($chapter->getId())?>">
+            <button class="btn btn-info">Voir le chapitre</button>
+        </a>
+        <a href="../public/index.php?page=single&chapterId=<?= htmlspecialchars($chapter->getId())?>">
+            <button class="btn btn-info">Modifier le chapitre</button>
+        </a>
+        <a href="../public/index.php?page=single&chapterId=<?= htmlspecialchars($chapter->getId())?>">
+            <button class="btn btn-danger">Supprimer le chapitre</button>
+        </a>
+    </div>
+</div>
+<?php } ?>
+
+<?php 
+    include_once './Views/Templates/footer.php'; 
+?>
