dc0aeb2ee848c6fd1f5d2452e80eeb28e0d2358e
Nicolas <niicolas.dubief@gmail.com>
2020-06-06 19:59:47 +0200

    Travail à approfondir

---
+++ Simple/Helpers/LoginsHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+class LoginsHelper
+{
+    public static function adminConnected()
+    {
+        
+    }
+}
--- Simple/Helpers/RouterHelper.php
+++ Simple/Helpers/RouterHelper.php
@@ -2,15 +2,29 @@
 
 class RouterHelper {
 
+    /**
+     * getPageIx
+     *
+     * @param mixed $get
+     * @return void
+     */
     public static function getPageIx($get) {
         $pageIx = 0;
+        var_dump($get);
 
         if (array_key_exists("pageIx", $get) && isset($get["pageIx"]) && is_numeric($get["pageIx"]) 
             && $get["pageIx"] >= 0) {
             $pageIx = $get["pageIx"];
+            var_dump($pageIx);
         }
 
         return $pageIx;
     }
 
+
+    public static function parseUrl($url)
+    {
+        $fragments = parse_url($url);
+        var_dump($fragments);
+    }
 }
