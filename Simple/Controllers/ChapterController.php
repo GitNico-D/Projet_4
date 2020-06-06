0beb1f4a3bc1311d1b374bfcea601a88a188fa39
Nicolas <niicolas.dubief@gmail.com>
2020-06-06 19:54:47 +0200

    Ajout de commentaire fonctionnel

--- Simple/Controllers/ChapterController.php
+++ Simple/Controllers/ChapterController.php
@@ -1,16 +1,14 @@
 <?php
 
-require_once "./Models/ChapterManager.php";
-require_once "./Models/CommentManager.php";
+require_once "./Controllers/Controller.php";
 
-class ChapterController
+class ChapterController extends Controller
 {
-    public function __construct()
-    {
-        $this->chapterManager = new ChapterManager();
-        $this->commentManager = new CommentManager();
-    } 
-
+    /**
+     * home
+     *
+     * @return void
+     */
     public function home()
     {
         $chaptersList = $this->chapterManager->getAllChapters();
@@ -24,14 +22,19 @@ class ChapterController
         require_once './Views/SingleView.php';
     }    
 
+    /**
+     * addNewChapter
+     *
+     * @return void
+     */
     public function addNewChapter()
     {
         echo ('Page addChapterView');
         if(!empty($_POST['chapterAuthor']) && !empty($_POST['chapterTitle']))
         {
-            $newChapterAuthor = $_POST['chapterAuthor'];
-            $newChapterTitle = $_POST['chapterTitle'];
-            $newChapterContent = $_POST['chapterContent'];
+            $newChapterAuthor = htmlspecialchars($_POST['chapterAuthor']);
+            $newChapterTitle = htmlspecialchars($_POST['chapterTitle']);
+            $newChapterContent = htmlspecialchars($_POST['chapterContent']);
             $affectedLines = $this->chapterManager->addChapterInDb($newChapterAuthor, $newChapterTitle, $newChapterContent);
             var_dump($affectedLines);
             if ($affectedLines === false)
@@ -49,5 +52,43 @@ class ChapterController
             echo 'Veuillez remplir les champs !';
         }
         require_once './Views/AddChapterView.php';
+    }
+
+    /**
+     * deleteChapter
+     *
+     * @param mixed $chapterId
+     * @return void
+     */
+    public function deleteChapter($chapterId)
+    {
+        $this->chapterManager->deleteChapterById($chapterId);
+        $this->home();
+    }
+
+    public function addComment($chapterId)
+    {
+        var_dump($chapterId);
+        if(!empty($_POST['commentAuthor']) && !empty($_POST['commentTitle']))
+        {
+            $commentAuthor = htmlspecialchars($_POST['commentAuthor']);
+            $commentTitle = htmlspecialchars($_POST['commentTitle']);
+            $commentContent = htmlspecialchars($_POST['commentContent']);
+            $commentAdded = $this->commentManager->addCommentOnChapter($commentAuthor, $commentTitle, $commentContent, $chapterId);
+            if ($commentAdded === false)
+            {
+                die('Impossible d\'ajouter le commentaire');
+            }
+            else 
+            {
+                $this->single($chapterId);
+                echo('Commentaire ajouté');
+            }
+        }
+        else
+        {   
+            echo 'Veuillez remplir les champs !';
+        }
+        
     }
 } 
