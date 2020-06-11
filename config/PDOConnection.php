12ef8bef397a49dafd895e384c4917f9214be628
Nicolas <niicolas.dubief@gmail.com>
2020-06-11 09:51:19 +0200

    Fichiers sauvegardé pour être remplacé par une architecture plus simple

--- config/PDOConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-CONST HOST = 'localhost';
-CONST DB_CHARSET = 'utf8';
-CONST DB_NAME = 'db_project_four';
-CONST DB_HOST = 'mysql:dbname=' . DB_NAME . ';host=' . HOST . ';charset=' . DB_CHARSET;
-CONST DB_USERNAME = 'root';
-CONST DB_PASSWORD = '';
--- view/errorView.php
+++
@@ -1,6 +0,0 @@
-<?php $pageTitle = 'PAGE NOT FOUND'; ?>
-<?php ob_start() ?>
-<!-- <?= $errorMessage; ?> -->
-<?php $content = ob_get_clean(); ?>
-
-<?php require 'defaultView.php' ?> 
