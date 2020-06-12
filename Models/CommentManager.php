bac9ac15dfe7acd86116c59850dce53d46e39c16
Nicolas <niicolas.dubief@gmail.com>
2020-06-12 17:22:00 +0200

    Suppression de lignes inutiles

--- Models/CommentManager.php
+++ Models/CommentManager.php
@@ -9,7 +9,6 @@ class CommentManager extends DAO
     public function getCommentByChapterId($chapterId)
     {
         $sqlRequest = 'SELECT * FROM comments WHERE chapterId = ? ORDER BY createdDate ASC';
-        var_dump($sqlRequest);
         $result = $this->createQuery($sqlRequest, [$chapterId]);
         $commentList = [];
         foreach ($result as $comment)
