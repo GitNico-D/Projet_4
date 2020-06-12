b0a161c29ff40b7c15d441b833a937eb2ad40418
Nicolas <niicolas.dubief@gmail.com>
2020-06-12 20:05:52 +0200

    Travail mise en forme

--- Views/AdminView.php
+++ Views/AdminView.php
@@ -3,37 +3,37 @@
     include_once "./Views/Templates/header.php";
 ?>
 
-<p class="lead text-muted">
-    Voici la liste des chapitres que vous avez publiés !
+<p class="lead text-muted my-5">
+    Voici les derniers chapitres que vous avez publiés !
 </p>
 
+<a href="./index.php?page=addNewChapter">
+    <button class="btn btn-info my-3">Écrire un chapitre</button>
+</a>
+
 <?php foreach($chaptersList as $chapter) {?>
-<div class=card>
-    <h2 class=card-header>
+<div class="card my-3">
+    <h2 class="card-header">
         <?= htmlspecialchars($chapter->getTitle())?>
     </h2>
-    <p class=card-body>
+    <p class="card-body">
         Posté le 
-        <span class=font-italic>
+        <span class="font-italic">
             <?= htmlspecialchars($chapter->getCreateDate())?>
         </span> 
-        par 
-        <span class=font-italic>
-            <?= htmlspecialchars($chapter->getAuthor())?>
-        </span>
     </p>
-    <p class= text-center-justify>
-        <?= htmlspecialchars($chapter->getContent())?>
+    <p class="text-center-justify">
+        <?= htmlspecialchars(substr($chapter->getContent(), 0, 500)); ?>
     </p>
     <div>
         <a href="./index.php?page=single&chapterId=<?= htmlspecialchars($chapter->getId())?>">
-            <button class="btn btn-info">Voir le chapitre</button>
+            <button class="btn btn-info my-3">Voir le chapitre</button>
         </a>
         <a href="./index.php?page=single&chapterId=<?= htmlspecialchars($chapter->getId())?>">
-            <button class="btn btn-info">Modifier le chapitre</button>
+            <button class="btn btn-info my-3">Modifier le chapitre</button>
         </a>
         <a href="./index.php?page=deleteChapter&chapterId=<?= htmlspecialchars($chapter->getId())?>">
-            <button class="btn btn-danger">Supprimer le chapitre</button>
+            <button class="btn btn-danger my-3">Supprimer le chapitre</button>
         </a>
     </div>
 </div>
