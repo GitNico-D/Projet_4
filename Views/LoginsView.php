b43537911f8f218bb7d127effd9e56e79b7b3935
Nicolas <niicolas.dubief@gmail.com>
2020-06-13 20:30:21 +0200

    Modification de mise en forme

--- Views/LoginsView.php
+++ Views/LoginsView.php
@@ -1,22 +1,25 @@
 <?php 
-    $pageTitle = 'Page de connection';
     require_once "./Views/Templates/header.php"; 
 ?>
 
-<div class="container">
-    <form action=<?="index.php?page=getLogin"?> method="post">
-        <div class="form-group">
-        <label for="email" class="bold">E-mail</label>
-        <input type="email" class="form-control" id="loginsEmail" name="loginsEmail">
-        <label for="password" class="bold margin-top">Mot de passe</label>
-        <input type="password" class="form-control" id="loginsPassword" name="loginsPassword"></input>
-        <button type="submit" class="btn btn-primary btn-block margin-top">Se connecter</button>
-    </form>
+<div class="form-signin-position text-center">
+    <div class="form-signin">
+        <form action=<?="index.php?page=getLogin"?> method="post" class="card">
+            <span class="fas fa-sign-in-alt fa-5x fa-signin"></span>
+            <h3 class="font-weight-normal">Connectez-vous</h3>
+            <label for="email" class="sr-only"></label>
+                <input type="email" class="form-control" id="loginsEmail" name="loginsEmail" placeholder="Adresse email">
+            <label for="password" class="sr-only"></label>
+                <input type="password" class="form-control" id="loginsPassword" name="loginsPassword" placeholder="Mot de passe"></input>
+            <button type="submit" class="btn btn-block btn-primary margin-top">Se connecter</button>
+            <p class="text-muted">© 2020</p>
+        </form>
+        <a href="./index.php">
+            <!-- <button class="btn btn-block btn-info margin-top mb-3"> -->
+            Retour à l'accueil
+            <!-- </button> -->
+        </a>
+    </div>
 </div>
-<a href="./index.php">
-    <button class="btn btn-block btn-info">
-    Retour à l'accueil
-    </button>
-</a>
 
 <?php require_once './Views/Templates/footer.php' ?>
