74132cc25f1addaeec42db063678d0e98f44d4c3
Nicolas <niicolas.dubief@gmail.com>
2020-06-18 19:13:25 +0200

    Mise en forme de la vue

--- Views/errorView.php
+++ Views/errorView.php
@@ -1,4 +1,15 @@
 <?php
-    $pageTitle = 'Page Introuvable!';
     include_once "./Views/Templates/header.php"; 
 ?>
+
+<section class="header-page text-center">
+    <img src="./src/data/img/404.jpg" alt="" class="header-page-img">
+    <div class="container jumbotron-container">
+        <div class="jumbotron">
+            <h1 class="display-4">Page Introuvable</h1>
+            <a href="./index.php"><button class="btn btn-info margin-top">Retour à l'accueil</button></a>
+        </div>
+    </div>
+</section>
+
+<?php include_once "./Views/Templates/footer.php" ?>
