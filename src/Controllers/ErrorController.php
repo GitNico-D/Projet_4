8f0c697b33685a550f9b99df99c00991c91baa02
Nicolas <niicolas.dubief@gmail.com>
2020-06-29 14:42:15 +0200

    Controller pour les erreurs permettant l'affichage des vue Twig

---
+++ src/Controllers/ErrorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\src\Controllers;
+
+use App\src\Core\Controller;
+
+class ErrorController extends Controller
+{
+//     public function __construct($message = null, $code = null)
+//     {
+//         $this->message = $message;
+//         $this->code = $code;
+//         var_dump($message, $code);
+//     }
+
+    public function error404($error)
+    {
+        echo $this->twig->render('Error404View.twig', ['error' => $error->getMessage()]);
+    }
+
+    public function error500()
+    {
+        echo $this->twig->render('Error500View.twig');
+    }
+}
