1d3b71248eba3d1c2843c0aa2eba80694d273f7f
Nicolas <niicolas.dubief@gmail.com>
2020-07-06 16:33:21 +0200

    Config Db fichier php

---
+++ config/dbConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+#Parameters for Database
+
+CONST HOST = "localhost";
+CONST DB_NAME = "db_project_four";
+CONST CHARSET = "utf8";
+CONST DB_USERNAME = 'root';
+CONST DB_PASSWORD = '';
+
+CONST DB_HOST = 'mysql:host=' . HOST . ';dbname=' . DB_NAME . ';charset=' . CHARSET;
+
