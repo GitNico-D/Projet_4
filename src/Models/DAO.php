0a61317633ba53f40a3e05e5bd365b73c67eec56
Nicolas <niicolas.dubief@gmail.com>
2020-07-06 16:34:50 +0200

    Change PDO Variable

--- src/Models/DAO.php
+++ src/Models/DAO.php
@@ -3,7 +3,6 @@
 namespace App\src\Models;
 
 use PDO;
-// use App\src\Core\PDOFactory;
 
 abstract class DAO 
 {
@@ -15,12 +14,9 @@ abstract class DAO
         {
             try
             {
-                // $this->db = new PDO(self::DB_HOST, self::DB_USERNAME, self::DB_PASSWORD);
-                $this->db = new PDO('mysql:host=localhost;dbname=db_project_four;charset=utf8', 'root', '');
+                $this->db = new PDO(DB_HOST, DB_USERNAME, DB_PASSWORD);
                 $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // echo 'Connection OK !';
                 return $this->db;
-                // PDOFactory::getMysqlConnection()
             }
             catch (Exception $errorConnexion)
             {
@@ -38,13 +34,11 @@ abstract class DAO
         if ($parameters)
         {
             $request = $this->getDb()->prepare($sql);
-            // $request->setFetchMode(PDO::FETCH_CLASS, static::class);
             // var_dump($parameters);
             $request->execute($parameters);
             return $request;
         }
         $request = $this->getDb()->query($sql);
-        // $request->setFetchMode(PDO::FETCH_CLASS, static::class);
         return $request;
     }
 
