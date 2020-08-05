2264a68c8642db7d428a336ef8f8ae95e8748ae1
Nicolas <niicolas.dubief@gmail.com>
2020-08-05 19:35:08 +0200

    Update based on parent manager

--- src/Managers/ChapterManager.php
+++ src/Managers/ChapterManager.php
@@ -26,6 +26,26 @@ class ChapterManager extends Manager
         return $publishedChaptersList;
     }
 
+    
+    // public function getAllPublishedChapters()
+    // {
+    //     // $publishedChaptersList = $this->findBy($this->table, array('published' => 1), array('createDate' => 'ASC'));
+    //     $sqlRequest = 'SELECT * FROM chapter WHERE published = ? ORDER BY createDate ASC';
+    //     var_dump($sqlRequest);
+
+    //     $result = $this->createQuery($sqlRequest, ['published' => 1]);
+    //     var_dump($result);
+    //     $publishedChaptersList = [];
+    //     // $entity = 'App\src\Models\\' . ucfirst($table);
+    //     foreach ($result as $data) {
+    //         var_dump($data);
+    //         $publishedChaptersList [] = new Chapter($data);
+    //     }
+    //     $result->closeCursor();
+    //     var_dump($publishedChaptersList);
+    //     return $publishedChaptersList;
+    // }
+
     public function getAllUnpublishedChapters()
     {
         $unpublishedChaptersList = $this->findBy($this->table, array('published' => 0), array('createDate' => 'ASC'), 10);
@@ -38,31 +58,53 @@ class ChapterManager extends Manager
         $this->createQuery($sqlRequest, [$chapterId]);
     }
 
-    public function addChapterInDb($newChapterAuthor, $newChapterTitle, $newChapterContent, $chapterPublished, $newChapterImg)
+    public function addChapterInDb($newChapterAuthor, $newChapterTitle, $newChapterContent, $chapterCreateDate, $chapterPublished, $newChapterImg)
     {
-        $sqlRequest = 'INSERT INTO chapter (author, title, content, createDate, updateDate, published, imgUrl) VALUES (:chapterAuthor, :chapterTitle, :chapterContent, NOW(), NOW(), :chapterPublished, :chapterImg)';
-        $affectedLines = $this->createQuery($sqlRequest, array(
-            'chapterAuthor' => $newChapterAuthor,
-            'chapterTitle' => $newChapterTitle,
-            'chapterContent' => $newChapterContent,
-            'chapterPublished' => $chapterPublished,
-            'chapterImg' => $newChapterImg
-        ));
-        return $affectedLines;
+        $insertLines = $this->insertInto($this->table, 
+            array('author', 'title', 'content', 'createDate', 'updateDate', 'published', 'imgUrl'),
+            array('chapterAuthor' => $newChapterAuthor, 
+                'chapterTitle' => $newChapterTitle,
+                'chapterContent' => $newChapterContent,
+                'chapterCreateDate' => $chapterCreateDate,
+                'chapterUpdateDate' => $chapterCreateDate,
+                'chapterPublished' => $chapterPublished,
+                'chapterImg' => $newChapterImg
+                ));
+        return $insertLines;
     }
 
-    public function updateChapterById($updatedChapterTitle, $updatedChapterContent, $chapterPublished, $updatedChapterImg, $chapterId)
+    // public function updateChapterById($updatedChapterTitle, $updatedChapterContent, $chapterPublished, $updatedChapterImg, $chapterId)
+    // {
+    //     $sqlRequest = 'UPDATE chapter SET title= :updatedChapterTitle, content= :updatedChapterContent, updateDate = NOW(), published= :chapterPublished, imgUrl= :updatedChapterImg WHERE id = :chapterId';
+    //     $updatedLines = $this->createQuery($sqlRequest, array(
+    //             'updatedChapterTitle' => $updatedChapterTitle,
+    //             'updatedChapterContent' => $updatedChapterContent,
+    //             'chapterPublished' => $chapterPublished,
+    //             'updatedChapterImg' => $updatedChapterImg,
+    //             'chapterId' => $chapterId
+    //             ));
+    //     return $updatedLines;
+    // }
+
+    public function updateChapterById($updatedChapterTitle, $updatedChapterContent, $updatedChapterDate, $chapterPublished, $updatedChapterImg, $chapterId)
     {
-        $sqlRequest = 'UPDATE chapter SET title= :updatedChapterTitle, content= :updatedChapterContent, updateDate = NOW(), published= :chapterPublished, imgUrl= :updatedChapterImg WHERE id = :chapterId';
-        $updatedLines = $this->createQuery($sqlRequest, array(
-                'updatedChapterTitle' => $updatedChapterTitle,
-                'updatedChapterContent' => $updatedChapterContent,
-                'chapterPublished' => $chapterPublished,
-                'updatedChapterImg' => $updatedChapterImg,
-                'chapterId' => $chapterId
-                ));
+        $updatedLines = $this->update($this->table, 
+                array('title' => 'updatedChapterTitle', 
+                    'content' => 'updatedChapterContent', 
+                    'updateDate' => 'updatedChapterDate',
+                    'published' => 'chapterPublished', 
+                    'imgUrl' => 'updatedChapterImg'),
+                array('updatedChapterTitle' => $updatedChapterTitle,
+                    'updatedChapterContent' => $updatedChapterContent,
+                    'updatedChapterDate' => $updatedChapterDate,                    
+                    'chapterPublished' => $chapterPublished,
+                    'updatedChapterImg' => $updatedChapterImg),
+                array('chapterId' => $chapterId)
+                );
         return $updatedLines;
     }
+
+    
 
     public function deleteChapterById($chapterId)
     {
