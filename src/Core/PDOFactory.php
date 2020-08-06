eb9d56ecc037cb5db387f4e2cb93cbd391bb696d
Nicolas <niicolas.dubief@gmail.com>
2020-08-06 20:14:12 +0200

    NC

--- src/Core/PDOFactory.php
+++ src/Core/PDOFactory.php
@@ -10,7 +10,6 @@ class PDOFactory
     {
         $db = new PDO('mysql:host=localhost;dbname=db_project_four;charset=utf8', 'root', '');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
         return $db;
     }
 }
