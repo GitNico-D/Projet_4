efa8af02ec692fd9c1c8021df9798bbc67d80fa3
Nicolas <niicolas.dubief@gmail.com>
2020-08-06 20:14:42 +0200

    Advanced on the parent manager

--- src/Core/Manager.php
+++ src/Core/Manager.php
@@ -19,104 +19,137 @@ class Manager extends DAO
         $sqlRequest = "SELECT * FROM " . $table . " WHERE ";
         // $whereArray = [];
         foreach ($where as $whereKey => $whereValue) {
-            $sqlRequest .= $whereKey . " = " . $whereValue;
+            $sqlRequest .= $whereKey . " = ?";
         }
-        $result = $this->createQuery($sqlRequest);
+        var_dump($sqlRequest);
+        $result = $this->createQuery($sqlRequest, $where);
         $requestResult = $result->fetch();
         $result->closeCursor();
-        return $requestResult;
-    }
-
-    /**
-     * findBy
-     *
-     * @param mixed $table
-     * @param mixed $where
-     * @param mixed $orderBy
-     * @param mixed $limit
-     * @param mixed $className
-     * @return void
-     */
-    public function findBy($table, $where, $orderBy, $limit = null)
-    {
-        $sqlRequest = 'SELECT * FROM ' . $table;
-        foreach ($where as $whereKey => $whereValue) {
-            $whereClause [] = $whereKey . ' = ' . $whereValue;
-        }
-        $sqlRequest .= ' WHERE ' . implode(' AND ', $whereClause);
-        if ($orderBy) {
-            foreach ($orderBy as $orderByKey => $orderByValue) {
-                $orderByArray [] = $orderByKey . ' ' . $orderByValue;
-            }
-            $sqlRequest .= ' ORDER BY ' . implode($orderByArray);
-        }
-        // if ($limit != null) {
-        //     $sqlRequest .= ' LIMIT ' . $limit;
-        // } 
-        // else {
-        //     $sqlRequest .= "'";
-        // }
-        // var_dump($sqlRequest);
-        $result = $this->createQuery($sqlRequest);
-        // var_dump($result);
-        $dataList = [];
         $entity = 'App\src\Models\\' . ucfirst($table);
-        foreach ($result as $data) {
-            // var_dump($data);
-            $dataList [] = new $entity($data);
-        }
-        $result->closeCursor();
-        return $dataList;
+        return new $entity($requestResult);
     }
 
+    // /**
+    //  * findBy
+    //  *
+    //  * @param mixed $table
+    //  * @param mixed $where
+    //  * @param mixed $orderBy
+    //  * @param mixed $limit
+    //  * @param mixed $className
+    //  * @return void
+    //  */
     // public function findBy($table, $where, $orderBy, $limit = null)
     // {
     //     $sqlRequest = 'SELECT * FROM ' . $table;
     //     var_dump($where);
-    //     $whereParameters = []; 
+    //     var_dump($orderBy);
     //     foreach ($where as $whereKey => $whereValue) {
-    //         // $whereKey = ? . $wherekey;
-    //         $whereKeyArray [] = $whereKey ;
-    //         var_dump($whereKeyArray);
-    //         foreach ($where as $whereKey => $whereValue)
-    //         {
-    //             $whereValueArray [] = $whereValue;
-    //             var_dump($whereValueArray);
-    //             $whereParameters [] = $whereKeyArray;
-    //         }
-            
-    //     } 
-    //     var_dump($whereParameters);
-    //     $sqlRequest .= ' WHERE whereKey = :whereValue, ';
-    //     var_dump($sqlRequest);
+    //         $whereClause [] = $whereKey . ' = ' . $whereValue;
+    //     }
+    //     $sqlRequest .= ' WHERE ' . implode(' AND ', $whereClause);
     //     if ($orderBy) {
     //         foreach ($orderBy as $orderByKey => $orderByValue) {
     //             $orderByArray [] = $orderByKey . ' ' . $orderByValue;
     //         }
     //         $sqlRequest .= ' ORDER BY ' . implode($orderByArray);
     //     }
-    //     if ($limit != null) {
-    //         $sqlRequest .= ' LIMIT ' . $limit;
-    //     } else {
-    //         $sqlRequest .= "'";
-    //     }
+    //     // if ($limit != null) {
+    //     //     $sqlRequest .= ' LIMIT ' . $limit;
+    //     // }
+    //     // else {
+    //     //     $sqlRequest .= "'";
+    //     // }
     //     // var_dump($sqlRequest);
-    //     $result = $this->createQuery($sqlRequest, array($whereKeyArray => $whereValueArray));
+    //     $result = $this->createQuery($sqlRequest);
+    //     // var_dump($result);
     //     $dataList = [];
-    //     $entity = 'App\src\Models\\' . ucfirst($table);;
+    //     $entity = 'App\src\Models\\' . ucfirst($table);
     //     foreach ($result as $data) {
+    //         // var_dump($data);
+    //         $dataList [] = new $entity($data);
+    //     }
+    //     $result->closeCursor();
+    //     return $dataList;
+    // }
+   
+    public function findBy($table, $where, $orderBy = null, $limit = null)
+    {
+        $sqlRequest = 'SELECT * FROM ' . $table;
+        var_dump($where);
+        var_dump($orderBy);
+        foreach ($where as $whereKey => $whereValue) {
+            $whereClause [] = $whereKey . ' = ' . $whereValue;
+        }
+        $sqlRequest .= ' WHERE ' . implode(' AND ', $whereClause);
+        // if ($orderBy) {
+        //     foreach ($orderBy as $orderByKey => $orderByValue) {
+        //         $orderByArray [] = $orderByKey . ' ' . $orderByValue;
+        //     }
+        //     $sqlRequest .= ' ORDER BY ' . implode($orderByArray);
+        // }
+        // if ($limit != null) {
+        //     $sqlRequest .= ' LIMIT ' . $limit;
+        // }
+        // else {
+        //     $sqlRequest .= "'";
+        // }
+        var_dump($sqlRequest);
+        $result = $this->createQuery($sqlRequest, $where);
+        var_dump($result);
+        $dataList = [];
+        $entity = 'App\src\Models\\' . ucfirst($table);
+        foreach ($result as $data) {
+            var_dump($data);
+            $dataList [] = new $entity($data);
+        }
+        $result->closeCursor();
+        return $dataList;
+    }
+
+    // public function findBy($table, $where, $orderBy = null, $limit = null)
+    // {
+    //     $sqlRequest = 'SELECT * FROM ' . $table;
+    //     var_dump($where);
+    //     var_dump($orderBy);
+    //     $whereArrayKey = [];
+    //     foreach ($where as $whereKey => $whereValue) {
+    //         $whereArrayKey [] = $whereKey . ' = ?';
+    //     }
+    //     $sqlRequest .= ' WHERE ' . implode(' AND ', $whereArrayKey);
+    //     var_dump($sqlRequest);
+    //     // if ($orderBy !== null) {
+    //     //     foreach ($orderBy as $orderByKey => $orderByValue) {
+    //     //         $orderByArray [] = $orderByKey . ' ' . $orderByValue;
+    //     //     }
+    //     //     $sqlRequest .= ' ORDER BY ' . implode($orderByArray);
+    //     // }
+    //     var_dump($sqlRequest);
+    //     // if ($limit !== null) {
+    //     //     $sqlRequest .= ' LIMIT ' . $limit;
+    //     // }
+    //     // else {
+    //     //     $sqlRequest .= "'";
+    //     // }
+    //     var_dump($sqlRequest);
+    //     $result = $this->createQuery($sqlRequest, $where);
+    //     var_dump($result);
+    //     $dataList = [];
+    //     $entity = 'App\src\Models\\' . ucfirst($table);
+    //     foreach ($result as $data) {
+    //         var_dump($data);
     //         $dataList [] = new $entity($data);
     //     }
     //     $result->closeCursor();
     //     return $dataList;
     // }
 
+
     /**
      * findAll
      *
      * @param mixed $table
-     * @param mixed $className
-     * @return void
+     * @return array
      */
     public function findAll($table)
     {
@@ -131,7 +164,27 @@ class Manager extends DAO
         return $dataList;
     }
 
-    /**
+    // /**
+    //  * insertInto
+    //  *
+    //  * @param mixed $table
+    //  * @param mixed $tableLines
+    //  * @param mixed $values
+    //  * @return void
+    //  */
+    // public function insertInto($table, $tableLines, $values)
+    // {
+    //     $sqlRequest = 'INSERT INTO ' . $table . " (" . implode(", ", $tableLines). ") VALUES (";
+    //     $arrayKey = [];
+    //     foreach ($values as $key => $value) {
+    //         $arrayKey [] = ":" . $key;
+    //     }
+    //     $sqlRequest .= implode(", ", $arrayKey) . ")";
+    //     $insertLines = $this->createQuery($sqlRequest, $values);
+    //     return $insertLines;
+    // }
+
+ /**
      * insertInto
      *
      * @param mixed $table
@@ -139,13 +192,14 @@ class Manager extends DAO
      * @param mixed $values
      * @return void
      */
-    public function insertInto($table, $tableLines, $values)
+    public function insertInto($table, $fields, $fieldValues)
     {
-        $sqlRequest = 'INSERT INTO ' . $table . " (" . implode(", ", $tableLines). ") VALUES (";
+        $
+        $sqlRequest = 'INSERT INTO ' . $table . " (" . implode(", ", $fields). ") VALUES (";
         $arrayKey = [];
-        foreach($values as $key => $value) {
+        foreach ($values as $key => $value) {
             $arrayKey [] = ":" . $key;
-            }
+        }
         $sqlRequest .= implode(", ", $arrayKey) . ")";
         $insertLines = $this->createQuery($sqlRequest, $values);
         return $insertLines;
@@ -158,12 +212,12 @@ class Manager extends DAO
         var_dump($setValues);
         $arraySet = [];
         $setArrayValues = [];
-        foreach($setLines as $setLinesKey => $setLinesValue) {
+        foreach ($setLines as $setLinesKey => $setLinesValue) {
             $arraySet [] = $setLinesKey . "= :" . $setLinesValue;
         }
         $sqlRequest .= implode(", ", $arraySet) . ")";
         var_dump($where);
-            // $setArrayValues[$key] = "$" . $value;
+        // $setArrayValues[$key] = "$" . $value;
         foreach ($where as $key => $value) {
             $key = ": " . $key;
         }
@@ -171,5 +225,14 @@ class Manager extends DAO
         var_dump($sqlRequest);
         $updatedLines = $this->createQuery($sqlRequest, $setValues);
         return $updatedLines;
+    }
+
+    public function delete($table, $where)
+    {
+        $sqlRequest = 'DELETE FROM' . $table;
+        foreach($where as $whereKey => $whereValue) {
+            $sqlRequest .= " WHERE " . $whereKey . "= ?";
+        }
+        $this->createQuery($sqlRequest, $where);
     }
 }
