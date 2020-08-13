cace6530ac85d585a5fa25d91f663c6cef17fe09
Nicolas <niicolas.dubief@gmail.com>
2020-08-13 19:49:54 +0200

    Need more explanation

--- src/Core/PDOFactory.php
+++ src/Core/PDOFactory.php
@@ -3,13 +3,41 @@
 namespace App\src\Core;
 
 use PDO;
-
+use Exception;
 class PDOFactory
 {
+    private $db;
+
     public static function getMysqlConnection()
     {
-        $db = new PDO('mysql:host=localhost;dbname=db_project_four;charset=utf8', 'root', '');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $db;
+        $dbConfig = yaml_parse_file('../config/db-config.yml');
+        if ($db === null) {
+            try {
+                $dbConfig = yaml_parse_file('../config/db-config.yml');
+                $db = new \PDO(
+                    'mysql:host=' . $dbConfig['DATABASE_HOST'] .';port=' . $dbConfig['DATABASE_PORT'] .
+                                    ';dbname=' . $dbConfig['DATABASE_NAME'] .';charset=' . $dbConfig['DATABASE_CHARSET'],
+                    $dbConfig['DATABASE_USERNAME'],
+                    $dbConfig['DATABASE_PASSWORD']
+                );
+                return $db;
+            } catch (Exception $errorConnexion) {
+                die('Erreur de connection : ' . $errorConnexion->getMessage());
+            }
+        } else {
+            return $db;
+        }
+    }
+
+    public function createQuery($sql, $parameters = null)
+    {
+        if ($parameters) {
+            var_dump($parameters);
+            $request = $this->getDb()->prepare($sql);
+            $request->execute($parameters);
+            return $request;
+        }
+        $request = $this->getDb()->query($sql);
+        return $request;
     }
 }
