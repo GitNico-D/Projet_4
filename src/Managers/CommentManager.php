8dcd378bc30491fd5663a35fffd857bc2f2b4c71
Nicolas <niicolas.dubief@gmail.com>
2020-08-20 15:18:02 +0200

    Modifying sql request

--- src/Managers/CommentManager.php
+++ src/Managers/CommentManager.php
@@ -113,13 +113,15 @@ class CommentManager extends Manager
      */
     public function getAllReportedComments()
     {
-        $sqlRequest = 'SELECT * FROM comment INNER JOIN reporting ON comment.id = reporting.commentId GROUP BY reporting.commentId';
+        $sqlRequest = 'SELECT comment.id, comment.author, comment.content, comment.createdDate, comment.chapterId, reporting.reportingDate
+                        FROM comment INNER JOIN reporting ON comment.id = reporting.commentId GROUP BY reporting.commentId';
         $result = $this->createQuery($sqlRequest);
         $reportedCommentList = [];
         foreach ($result as $comment) {
             $reportedCommentList [] = new Comment($comment);
         }
         $result->closeCursor();
+        var_dump($reportedCommentList);
         return $reportedCommentList;
     }
 
