f5b092606c8b7ae49b178e5ef80ab4ff98da97f6
Nicolas <niicolas.dubief@gmail.com>
2020-09-01 19:57:58 +0200

    25% work

--- src/Services/FormVerificationHelper.php
+++ src/Services/FormVerificationHelper.php
@@ -9,65 +9,59 @@ class FormVerificationHelper
     public static function checkField($post) 
     {
         var_dump($post);
-        // $postNull = [];
         foreach ($post as $postKey => $postValue) {
-            $value = str_replace('logins', '', $postKey);
             var_dump($postKey);
             var_dump($postValue);
-
-            var_dump($value);
-                switch ($value) {
-                    case "author":
-                        $error = self::checkFieldAuthor($postValue);
-                    break;
-                    case "title":
-                        $error = self::checkFieldTitle($postValue);
-                    break;
-                    case "content":
-                        $error = self::checkFieldContent($postValue);
-                    break;
-                    default;
-                }
+            if ($postKey == "author") {
+                $error = self::checkFieldAuthor($postValue);
+            } elseif ($postKey == "title") {
+                $error = self::checkFieldTitle($postValue);
+            } elseif ($postKey == "content") {
+                $errors = self::checkFieldContent($postValue);
             }
-            var_dump($error);
             return $error;
+                // switch ($postKey) {
+                //     case "author":
+                //         return $error;
+                //     break;
+                //     case "title":
+                //         $error = self::checkFieldTitle($postValue);
+                //     break;
+                //     case "content":
+                //         return $error;
+                //     break;
+                //     default;
+                // }
+            }
+            // var_dump($error);
+            // return $error;
                 // throw new Exception ('Les champs ' . implode(', ', $postNull) .  ' ne sont pas remplis');
     }
 
     public static function checkFieldAuthor($postAuthorValue)
     {
-        if (empty($postAuthorValue)) {
-            var_dump($postAuthorValue);
-            return "Le nom de l'auteur n'est pas indiqué";
-        }
         if (strlen($postAuthorValue) < 2) {
-            return "Le nom de l'auteur est trop court, minimum 2 caractères";
+            return "Le pseudonyme est trop court, minimum 2 caractères";
         }
         if (strlen($postAuthorValue) > 50) {
-            return "Le nom de l'auteur est trop long, maximum 50 caractères";
+            return "Le pseudonyme est trop long, maximum 50 caractères";
         }
     }
 
     private static function checkFieldTitle($title)
     {
-        if (empty($title)) {
-            return "Le nom de l'auteur n'est pas indiqué";
-        }
         if (strlen($title) < 2) {
-            return "Le nom de l'auteur est trop court, minimum 2 caractères";
+            return "Le titre est trop court, minimum 2 caractères";
         }
         if (strlen($title) > 255) {
-            return "Le nom de l'auteur est trop long, maximum 255 caractères";
+            return "Le titre est trop long, maximum 255 caractères";
         }
     }
 
     private static function checkFieldContent($content)
     {
-        if(empty($content)) {
-            return "Le nom de l'auteur n'est pas indiqué";
-        }
         if(strlen($content) < 5) {
-            return "Le nom de l'auteur est trop court, minimum 5 caractères";
+            return "Votre message doit contenir au moins 5 caractères";
         }
     }
 }
